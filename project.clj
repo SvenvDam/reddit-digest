59f7ef399e38f8ff2ade43fa84abae247479ba8c
Sven van Dam <sven.dam@protonmail.com>
2021-01-06 09:25:58 +0100

    Add test

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject reddit-digest "0.1.0-SNAPSHOT"
   :description "Digest for Reddit. Retrieves top posts for a set of subreddits and sends a newsletter."
-  :url "http://example.com/FIXME"
+  :url "https://github.com/SvenvDam/reddit-digest"
   :license {:name "MIT"
             :url  "https://mit-license.org/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
@@ -11,5 +11,6 @@
                  [environ "1.2.0"]]
   :main ^:skip-aot reddit-digest.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}}
+  :profiles {:uberjar {:aot :all}
+             :test {:resource-paths ["test-resources"]}}
   :uberjar-name "reddit-digest.jar")
--- test/reddit_digest/core_test.clj
+++ test/reddit_digest/core_test.clj
@@ -1,7 +1,19 @@
 (ns reddit-digest.core-test
   (:require [clojure.test :refer :all]
-            [reddit-digest.core :refer :all]))
+            [reddit-digest.core :refer :all]
+            [clojure.data.json :as json]
+            [clojure.java.io :as io]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-clean-post-data
+  (let [response-data (json/read-str (slurp (io/file (io/resource "response.json"))) :key-fn keyword)
+        posts-data (map :data (-> response-data :data :children))
+        cleaned-posts-data (map clean-post-data posts-data)
+        top-post (first cleaned-posts-data)
+        expected {:title        "Rust Design Patterns now also as a book"
+                  :score        765
+                  :comments     59
+                  :url          "https://www.reddit.com/r/rust/comments/kowtqn/rust_design_patterns_now_also_as_a_book/"
+                  :comment-link "https://www.reddit.com/r/rust/comments/kowtqn/rust_design_patterns_now_also_as_a_book/"}]
+    (testing "clean-post-data extracts and formats data from API response"
+      (is (= top-post expected)))))
+
